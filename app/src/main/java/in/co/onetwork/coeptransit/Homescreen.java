8d86a358f39ae30838cc67da7dd05000596a3468
Abhi <abhidashing.singh@gmail.com>
2017-09-17 03:38:04 +0530

    Added Map Activity

--- app/src/main/java/in/co/onetwork/coeptransit/Homescreen.java
+++ app/src/main/java/in/co/onetwork/coeptransit/Homescreen.java
@@ -4,6 +4,7 @@ import android.*;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -13,7 +14,9 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,14 +25,30 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.security.Provider;
+import java.util.ArrayList;
+import java.util.List;
+
+import in.co.onetwork.coeptransit.POJO.Example;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     GPSTracker gps;
     Double latitude,longitude;
+    LatLng origin;
+    LatLng dest;
+    ArrayList<LatLng> MarkerPoints;
+    TextView ShowDistanceDuration;
+    Polyline line;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +58,7 @@ public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         gps=new GPSTracker(this);
+        ShowDistanceDuration=(TextView)findViewById(R.id.textid);
     }
 //Abhi says no
 
@@ -54,11 +74,16 @@ public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (gps.canGetLocation()){
+            latitude=gps.getLatitude();
+            longitude=gps.getLongitude();
+            Toast.makeText(this, "Location:"+latitude+" "+longitude, Toast.LENGTH_SHORT).show();
+            origin =new LatLng(latitude,longitude);
+            mMap.addMarker(new MarkerOptions().position(origin).title("my"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin,15));
+        }else{
+            gps.showSettingsAlert();
+        }
     }
 
     public void locate(View v) {
@@ -66,11 +91,98 @@ public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
             latitude=gps.getLatitude();
             longitude=gps.getLongitude();
             Toast.makeText(this, "Location:"+latitude+" "+longitude, Toast.LENGTH_SHORT).show();
-            LatLng my =new LatLng(latitude,longitude);
-            mMap.addMarker(new MarkerOptions().position(my).title("my"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(my,15));
+            origin =new LatLng(latitude,longitude);
+            mMap.addMarker(new MarkerOptions().position(origin).title("my"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin,15));
         }else{
             gps.showSettingsAlert();
         }
+    }
+    public void drive(View v){
+        dest =new LatLng(18.5294,73.8566);
+        build_retrofit_and_get_response("driving");
+    }
+    private void build_retrofit_and_get_response(String type) {
+
+        String url = "https://maps.googleapis.com/maps/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitMaps service = retrofit.create(RetrofitMaps.class);
+
+        Call<Example> call = service.getDistanceDuration("metric", origin.latitude + "," + origin.longitude,dest.latitude + "," + dest.longitude, type);
+        Toast.makeText(Homescreen.this, "in method", Toast.LENGTH_SHORT).show();
+        call.enqueue(new Callback<Example>() {
+            @Override
+            public void onResponse(Response<Example> response, Retrofit retrofit) {
+
+                try {
+                    //Remove previous line from map
+                    if (line != null) {
+                        line.remove();
+                    }
+                    // This loop will go through all the results and add marker on each location.
+                    for (int i = 0; i < response.body().getRoutes().size(); i++) {
+                        String distance = response.body().getRoutes().get(i).getLegs().get(i).getDistance().getText();
+                        String time = response.body().getRoutes().get(i).getLegs().get(i).getDuration().getText();
+                        Toast.makeText(Homescreen.this, "this", Toast.LENGTH_SHORT).show();
+                        ShowDistanceDuration.setText("Distance:" + distance + ", Duration:" + time);
+                        String encodedString = response.body().getRoutes().get(0).getOverviewPolyline().getPoints();
+                        List<LatLng> list = decodePoly(encodedString);
+                        line = mMap.addPolyline(new PolylineOptions()
+                                .addAll(list)
+                                .width(20)
+                                .color(Color.RED)
+                                .geodesic(true)
+                        );
+                    }
+                } catch (Exception e) {
+                    Log.d("onResponse", "There is an error");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("onFailure", t.toString());
+                Toast.makeText(Homescreen.this, " "+t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
     }
 }
