92d3d9b6836b7233e2a86b6df41ebe8484de198d
Abhi <abhidashing.singh@gmail.com>
2017-09-17 00:31:57 +0530

    Added Map Activity

--- app/src/main/java/in/co/onetwork/coeptransit/Homescreen.java
+++ app/src/main/java/in/co/onetwork/coeptransit/Homescreen.java
@@ -1,7 +1,20 @@
 package in.co.onetwork.coeptransit;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,10 +23,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.Provider;
+
 public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    GPSTracker gps;
+    Double latitude,longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +38,7 @@ public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        gps=new GPSTracker(this);
     }
 
 
@@ -42,5 +59,18 @@ public class Homescreen extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void locate(View v) {
+        if (gps.canGetLocation()){
+            latitude=gps.getLatitude();
+            longitude=gps.getLongitude();
+            Toast.makeText(this, "Location:"+latitude+" "+longitude, Toast.LENGTH_SHORT).show();
+            LatLng my =new LatLng(latitude,longitude);
+            mMap.addMarker(new MarkerOptions().position(my).title("my"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(my,15));
+        }else{
+            gps.showSettingsAlert();
+        }
     }
 }
